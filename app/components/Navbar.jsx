54f3bfa3a81a69034319a06f427564d1d58947d6
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-12-14 19:50:10 +0530

    fix: navabr position error fixed

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   
   return (
-    <div className="sticky top-0 flex justify-between p-4 px-8 backdrop-blur-sm bg-black/20 z-50">
+    <div className="fixed top-0 left-0 right-0 flex justify-between p-4 px-8 backdrop-blur-sm bg-black/20 text-white z-50">
       <div className="text-2xl font-bold">Team Ignition</div>
       <div className="flex space-x-8 justify-center items-center">
         <a href="#about">About</a>
