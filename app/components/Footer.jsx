5e0e33eff2cbc0e13598fde538a3e418adabb6f2
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-12-15 00:01:26 +0530

    feat: enhance About and Footer components with animations and improved styling

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,46 +1,50 @@
 import React from "react";
 
-const Footer = () => {
+const Footer = ({ className = "" }) => {
   return (
-    <div className="p-4 w-full overflow-hidden">
-      <div className="w-full backdrop-blur-sm bg-white/10 text-white rounded-2xl pb-2">
-        <div className="flex justify-between p-8 px-8 h-full">
-          <div className="bg-white text-4xl text-black p-4 px-8 rounded-lg h-36 flex justify-center items-center font-bold">
-            Team Ignition
-          </div>
-          <div className="flex gap-8 p-4">
-            <div className="">
-              <div className="">Socails</div>
-              <div className="">Socails</div>
-              <div className="">Socails</div>
-              <div className="">Socails</div>
-              <div className="">Socails</div>
-            </div>
-            <div className="">
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-            </div>
-            <div className="">
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-            </div>
-            <div className="">
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-              <div className="">Contacts</div>
-            </div>
-          </div>
+    <div
+      className={`p-4 pb-12 w-full overflow-hidden bg-white text-black ${className}`}
+    >
+      <div className="flex justify-between p-8 px-8 h-full">
+        <div className="text-4xl bg-black text-white  rounded-lg h-44 w-72 p-4 flex flex-col justify-between">
+          <div className="font-bn">CONTACT US</div>
+          <div className="font-bold w-full text-right">Team Ignition</div>
         </div>
-        <div className="w-full text-center mb-4">
-          Copyright 2024 © Team Ignition. All Rights Reserveed.
+        <div className="flex gap-16">
+          <div className="flex">
+            <div className="mr-6 pt-2">LINKS</div>
+            <div className="overflow-hidden">
+              <div className="bg-black/10 text-black/60 border border-black/10 w-44 text-center p-2 rounded-t-lg">
+                Instagram
+              </div>
+              <div className="bg-black/10 text-black/60 border border-black/10 w-44 text-center p-2">
+                Instagram
+              </div>
+              <div className="bg-black/10 text-black/60 border border-black/10 w-44 text-center p-2">
+                Instagram
+              </div>
+              <div className="bg-black/10 text-black/60 border border-black/10 w-44 text-center p-2 rounded-b-lg">
+                Instagram
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="mr-6 pt-2">LINKS</div>
+            <div className="overflow-hidden">
+              <div className="bg-black/10 text-black/60 border border-black/10 w-44 text-center p-2 rounded-t-lg">
+                Instagram
+              </div>
+              <div className="bg-black/10 text-black/60 border border-black/10 w-44 text-center p-2">
+                Instagram
+              </div>
+              <div className="bg-black/10 text-black/60 border border-black/10 w-44 text-center p-2">
+                Instagram
+              </div>
+              <div className="bg-black/10 text-black/60 border border-black/10 w-44 text-center p-2 rounded-b-lg">
+                Instagram
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
