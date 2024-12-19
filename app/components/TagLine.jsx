d7765938d0618f3c40071a94a8fa6cb321e7b1d7
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-12-19 10:57:28 +0530

    fix: fixed tagline section overflow

--- app/components/TagLine.jsx
+++ app/components/TagLine.jsx
@@ -5,27 +5,25 @@ import BlurIn from "@/components/ui/blur-in";
 const TagLine = () => {
   return (
     <div className="relative py-72 text-[30px] font-bn border border-white/20 border-dashed w-full text-center overflow-hidden">
-      <div>
-        <Image
-          src="/models/rocket-model.png"
-          alt="Description of image"
-          layout="fill"
-          objectFit="contain"
-          className="invert -translate-x-[1050px]"
-        />
-        <Image
-          src="/models/rocket-model.png"
-          alt="Description of image"
-          layout="fill"
-          objectFit="contain"
-          className="invert translate-x-[1050px]"
-        />
-      </div>
+      <Image
+        src="/models/rocket-model.png"
+        alt="Description of image"
+        layout="fill"
+        objectFit="contain"
+        className="invert absolute left-0 translate-x-[80vw]"
+      />
+      <Image
+        src="/models/rocket-model.png"
+        alt="Description of image"
+        layout="fill"
+        objectFit="contain"
+        className="invert absolute right-0 -translate-x-[80vw]"
+      />
       <div className="relative z-10">
         <BlurIn
           word="Fueling Innovation, Reaching New Heights in Rocketry."
           duration="0.5"
-          className="text-[30px]"
+          className="text-[2.5vw]"
         />
       </div>
     </div>
